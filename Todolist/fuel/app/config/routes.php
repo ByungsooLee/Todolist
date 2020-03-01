dbfdfca61beb1c673ac9b4dbdf2aaccf9f3bef82
ByungsooLee <workout4050@gmail.com>
2020-03-02 00:42:39 +0800

    home layout

--- Todolist/fuel/app/config/routes.php
+++ Todolist/fuel/app/config/routes.php
@@ -18,7 +18,10 @@ return array(
 	 *
 	 */
 
-	'_root_' => 'welcome/index',
+	'_root_'  => 'welcome/index',  // The default route 
+	'_404_'   => 'welcome/404', // The main 404 route 
+
+	'hello(/:name)?' => array('welcome/hello', 'name' => 'hello'), 
 
 	/**
 	 * -------------------------------------------------------------------------
