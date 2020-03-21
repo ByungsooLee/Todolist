7a2da3e997bdfc6bd23c0d79e1f79a5c45f8e474
ByungsooLee <workout4050@gmail.com>
2020-03-21 12:40:42 +0900

    migration

--- Todolist/fuel/app/views/welcome/header.php
+++ Todolist/fuel/app/views/welcome/header.php
@@ -2,11 +2,11 @@
 <?php echo Asset::css('base.css'); ?>
 <div class="container">
   <h1 class="headline">
-    <a onclick="location.href='index'">HOME</a>
+    <a onclick="location.href='/'">HOME</a>
   </h1>
   <ul class="nav-list">
-    <li onclick="location.href='signup.php' " class="nav-list-item">Sign Up</li>
-    <li onclick="location.href='login.php' " class="nav-list-item">Log In</li>
+    <li onclick="location.href='signup' " class="nav-list-item">Sign Up</li>
+    <li onclick="location.href='login' " class="nav-list-item">Log In</li>
   </ul>
-</div>
+</div> 
   
